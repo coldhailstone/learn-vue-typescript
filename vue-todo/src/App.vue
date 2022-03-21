94f8c3764719bfaa35055b5d0ee65af7946228b2
cold_hailstone <cksn1993@gmail.com>
2022-03-21 14:34:20 +0900

    인풋 컴포넌트의 emit 이벤트 정의 및 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<h1>Vue Todo with TypeScript</h1>
-		<todo-input :item="todoText"></todo-input>
+		<todo-input :item="todoText" @input="updateTodoText"></todo-input>
 	</div>
 </template>
 
@@ -15,6 +15,11 @@ export default Vue.extend({
 		return {
 			todoText: '',
 		};
+	},
+	methods: {
+		updateTodoText(value: any) {
+			this.todoText = value;
+		},
 	},
 });
 </script>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<label for="todo-input"></label>
-		<input id="todo-input" type="text" :value="item" />
+		<input id="todo-input" type="text" :value="item" @input="handleInput" />
 		<button type="button" @click="addTodo">add</button>
 	</div>
 </template>
@@ -17,6 +17,9 @@ export default Vue.extend({
 		},
 	},
 	methods: {
+		handleInput(event: any) {
+			this.$emit('input', event.target.value);
+		},
 		addTodo() {
 			console.log('add');
 		},
