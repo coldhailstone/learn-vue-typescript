0aee2ab7cc9dceeb2ab43a2c5ba5840c9861f3b6
cold_hailstone <cksn1993@gmail.com>
2022-04-02 16:00:34 +0900

    state 타입 정의

--- vue-news/src/store/index.ts
+++ vue-news/src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { state } from './state';
 import getters from './getters.js';
 import mutations from './mutations.js';
 import actions from './actions.js';
@@ -8,14 +9,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	strict: process.env.NODE_ENV !== 'production',
-	state: {
-		news: [],
-		ask: [],
-		jobs: [],
-		user: {},
-		item: {},
-		list: [],
-	},
+	state,
 	getters,
 	mutations,
 	actions,
